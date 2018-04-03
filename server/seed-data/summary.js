d2910ef144a018e7de3c0333fbe1561da1f87cd0
Michael Schreier <mjschreier1@gmail.com>
2018-04-03 13:16:44 -0600

    fixed typo in README and updated seed data

--- server/seed-data/summary.js
+++ server/seed-data/summary.js
@@ -5,7 +5,6 @@ let data = [
     year: 2018,
     gross_revenue: 103.29,
     total_fees: 3.29,
-    net_revenue: this.gross_revenue - this.total_fees,
     unpaid_balance: 100
   },
   {
@@ -14,7 +13,6 @@ let data = [
     year: 2018,
     gross_revenue: 144.49,
     total_fees: 4.49,
-    net_revenue: this.gross_revenue - this.total_fees,
     unpaid_balance: 0
   },
   {
@@ -23,7 +21,6 @@ let data = [
     year: 2018,
     gross_revenue: 0,
     total_fees: 0,
-    net_revenue: this.gross_revenue - this.total_fees,
     unpaid_balance: 730
   }
 ];
